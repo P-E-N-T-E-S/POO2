b9a93877aa20a3abf2e1c443a47e261a33742c87
Sofia Saraiva <112433998+Sofia-Saraiva@users.noreply.github.com>
2024-04-12 17:23:38 -0300

    cliente e voo mediator

---
+++ AtividadeContinuadaPOO/src/br/edu/cesarschool/cc/poo/ac/passagem/VooMediator.java
@@ -0,0 +1,89 @@
+package br.edu.cesarschool.cc.poo.ac.passagem;
+import br.edu.cesarschool.cc.poo.ac.utils.StringUtils;
+
+import java.util.Objects;
+
+public class VooMediator {
+    VooDAO vooDao;
+
+    public Voo buscar(String IdVoo) {
+        return vooDao.buscar(IdVoo);
+    }
+
+    public String validarCiaNumero(String companhiaAerea, int numeroVoo) {
+        if (StringUtils.isVaziaOuNula(companhiaAerea) && companhiaAerea.length() < 2) {
+            return "CIA aerea errada";
+        }
+
+        if (numeroVoo < 0 || numeroVoo < 1000 || numeroVoo > 9999) {
+            return "Numero voo errado";
+        }
+
+        return null;
+    }
+
+    public String validar(Voo voo) {
+        String[] aeroportos = {
+                "GRU", "CGH", "GIG", "SDU", "REC", "CWB",
+                "POA", "BSB", "SSA", "FOR", "MAO", "SLZ",
+                "CNF", "BEL", "JPA", "PNZ", "CAU", "FEN",
+                "SET", "NAT", "PVH", "BVB", "FLN", "AJU",
+                "PMW", "MCZ", "MCP", "VIX", "GYN", "CGB",
+                "CGR", "THE", "RBR", "VCP", "RAO"
+        };
+
+        if (StringUtils.isVaziaOuNula(voo.getAeroportoOrigem())) {
+            return "Aeroporto origem errado";
+        }
+
+        if (StringUtils.isVaziaOuNula(voo.getAeroportoDestino())) {
+            return "Aeroporto destino errado";
+        }
+
+        if (Objects.equals(voo.getAeroportoOrigem(), voo.getAeroportoDestino())) {
+            return "Aeroporto origem igual a aeroporto destino";
+        }
+    }
+
+    public String incluir(Voo voo) {
+        if (validar(voo) != null) {
+            return validar(voo);
+        }
+        else {
+            if (!vooDao.incluir(voo)) {
+                return "Voo ja existente";
+            }
+            else {
+                return null;
+            }
+        }
+    }
+
+    public String alterar(Voo voo) {
+        if (validar(voo) != null) {
+            return validar(voo);
+        }
+        else {
+            if (!vooDao.alterar(voo)) {
+                return "Voo existente";
+            }
+            else {
+                return null;
+            }
+        }
+    }
+
+    public String excluir(String idVoo) {
+        if (StringUtils.isVaziaOuNula(idVoo)) {
+            return "Id voo errado";
+        }
+        else {
+            if (!vooDao.excluir(idVoo)) {
+                return "Voo inexistente";
+            }
+            else {
+                return null;
+            }
+        }
+    }
+}
