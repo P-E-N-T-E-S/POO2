d2af3d2ab827db89fd7929fdb592e2d62ac189f9
Sofia Saraiva <112433998+Sofia-Saraiva@users.noreply.github.com>
2024-04-12 17:36:07 -0300

    Update ClienteMediator.java

--- AtividadeContinuadaPOO/src/br/edu/cesarschool/cc/poo/ac/cliente/ClienteMediator.java
+++ AtividadeContinuadaPOO/src/br/edu/cesarschool/cc/poo/ac/cliente/ClienteMediator.java
@@ -1,23 +1,57 @@
 package br.edu.cesarschool.cc.poo.ac.cliente;
+import br.edu.cesarschool.cc.poo.ac.passagem.Voo;
 import br.edu.cesarschool.cc.poo.ac.utils.*;
 
 public class ClienteMediator {
 	ClienteDAO clienteDAO;
-	
-	public ClienteMediator() {
-		
-	}
-	
+
     public Cliente buscar(String cpf) {
-        return clienteDAO.buscar(cpf);
+		return clienteDAO.buscar(cpf);
     }
     
     public String validar(Cliente cliente) {
     	if (!ValidadorCPF.isCpfValido(cliente.getCpf())) {
     		return "CPF errado";
     	}
-    	else if ()
+
+		if (StringUtils.isVaziaOuNula(cliente.getNome()) || cliente.getNome().length() < 2) {
+			return "nome errado";
+		}
+
+		if (cliente.getSaldoPontos() < 0) {
+			return "saldo errado";
+		}
+
+		return null;
     }
+
+	public String incluir(Cliente cliente) {
+		if (validar(cliente) != null) {
+			return validar(cliente);
+		}
+		else {
+			if (!clienteDAO.incluir(cliente)) {
+				return "Cliente ja existente";
+			}
+			else {
+				return null;
+			}
+		}
+	}
+
+	public String excluir(String cpf) {
+		if (!ValidadorCPF.isCpfValido(cpf)) {
+			return "CPF errado";
+		}
+		else {
+			if (clienteDAO.excluir(cpf)) {
+				return null;
+			}
+			else {
+				return "Cliente inexistente";
+			}
+		}
+	}
     
     
 }
