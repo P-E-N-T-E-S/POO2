46535477454233860572f92c70f29244c4f5cf84
Sofia Lima <sofia19lima@gmail.com>
2024-05-27 10:58:31 -0300

    try except cpf

---
+++ AtividadeContinuadaPOO/src/br/edu/cesarschool/cc/poo/ac/utils/CpfInvalidoException.java
@@ -0,0 +1,7 @@
+package br.edu.cesarschool.cc.poo.ac.utils;
+
+public class CpfInvalidoException extends Exception {
+    public CpfInvalidoException(String message) {
+        super(message);
+    }
+}
